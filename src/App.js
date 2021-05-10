e4b201a0484037a13147ea0d4ca333cc90befff8
skarlinski <skarlinski@gmail.com>
2021-05-10 12:39:00 +0300

    Load users from JSON

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import RecipesPage from './pages/RecipesPage';
 import HomePage from './pages/HomePage';
 import RecipesNavbar from './components/RecipesNavbar';
 import { Container } from 'react-bootstrap';
+import userJSON from './data/users.json';
 
 // App is the main component for our Recipe app
 // State:
@@ -17,9 +18,14 @@ import { Container } from 'react-bootstrap';
 
 
 class App extends React.Component{
+  
     constructor(props) {
       super(props);
+      let usersData;
+      if( localStorage['allUsers']) {usersData= localStorage['allUsers'];}
+      else {usersData = userJSON}
       this.state = {
+         allUsers: usersData,
          activeUser: null
         // activeUser: {
         //   id: 1,
@@ -29,8 +35,19 @@ class App extends React.Component{
         // }
       }
     }
+    signup = (newUser) =>{
+      this.setState({
+        activeUser: newUser,
+        allUsers: this.state.allUsers.concat(newUser)
+      })
+      // localStorage['allUsers'] = localStorage['allUsers'].push(newUser) // Will not work sas localStorage is string
 
-
+    }
+    login = (userObj) => {
+      this.setState({
+        activeUser: userObj
+      });
+    }
     logout = () => {
       this.setState({
         activeUser: null,
@@ -56,7 +73,10 @@ class App extends React.Component{
             </RecipesPage>
           </Route>
           <Route exact path="/login">
-            <Login></Login>
+            <Login
+              allUsers={this.state.allUsers}
+              login={this.login}
+            />
           </Route>
           <Route exact path="/signup"> 
             <Signup></Signup>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,19 +4,57 @@ import { Link } from 'react-router-dom';
 
 class Login extends React.Component{
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            email: '',
+            pwd: ''
+        }
+    }
+    updatePass = (event) => {
+        this.setState({
+            pwd: event.target.value
+        })
+    }
+    updateEmail = (event) =>{
+        this.setState({
+            email: event.target.value
+        })
+    }
+    handleLogin = () => {
+        // Checks the typed username and password from the controlled compoenents
+        // check if user exists in this.props.allUsers, and if the password is correct
+        // if true return the user object
+        // if false - show alert - password / user not found
+        const foundUser = this.props.allUsers.find( (user) => {
+            return user.email === this.state.email && user.pwd === this.state.pwd;
+        });
+        // for(let i=0; i<this.props.allUsers.length; i++) {
+        //     if(user.email === this.state.email && user.pwd === this.state.pwd){
+        //         foundUser = this.props.allUsers[i];
+        //         break;
+        //     }
+        // }
+
+        if(foundUser) {
+            this.props.login(foundUser);
+            window.location.href = '/#/recipes'
+        }
+        else{
+            alert(" Incorrect Email or Password");
+        }
+
     }
     render(){
         return (
         <div className="p-login">
             <h1> Login to My Recipe Book</h1>
             <Form className="mt-5">
-            <Form.Group as={Row} controlId="formHorizontalEmail">
+            <Form.Group as={Row} controlId="formHorizontalEmail" >
                 <Form.Label column sm={2}>
                 Email
                 </Form.Label>
                 <Col sm={10}>
-                <Form.Control type="email" placeholder="Email" />
+                <Form.Control type="email" placeholder="Email" name="email" onChange={this.updateEmail} value={this.state.email}/>
                 </Col>
             </Form.Group>
 
@@ -25,13 +63,13 @@ class Login extends React.Component{
                 Password
                 </Form.Label>
                 <Col sm={10}>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" placeholder="Password" name="password" onChange={this.updatePass} value={this.state.password}/>
                 </Col>
             </Form.Group>
            
             <Form.Group as={Row}>
                 <Col sm={{ span: 10, offset: 2 }}>
-                <Button variant="success" type="button">Log in</Button>
+                <Button onClick={this.handleLogin} variant="success" type="button">Log in</Button>
                 </Col>
             </Form.Group>
             </Form>
