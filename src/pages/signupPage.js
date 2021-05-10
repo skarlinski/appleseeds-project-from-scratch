068c491e21bfc5a4b195ae19947d439288194ccd
skarlinski <skarlinski@gmail.com>
2021-05-10 09:45:15 +0300

    reset

--- src/pages/signupPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-class SignupPage extends React.Component {
-    render() {
-        return (
-            <div className="c-signup-page">signup</div>
-        )
-    }
-}
-export default SignupPage;
